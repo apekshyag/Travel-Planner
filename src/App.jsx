5e1739ba29bd374113ceefa148f23bd83221a694
apekshyag <apekshyaghimire41@gmail.com>
2025-08-29 13:36:44 +0545

    login page made and routing for login and signup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import { BrowserRouter, Route,Routes } from 'react-router-dom'
 import LandingPage from './Pages/LandingPage';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
 
 function App(){
   return(
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />}/>
     </Routes>
     </BrowserRouter>
   );
